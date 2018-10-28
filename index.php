fe76129837d39d006c1998c4fe4f03c6b3782f3e
user <esukna@gmail.com>
2018-10-28 14:15:37 +0300

    add package.json for front-end install bootstrap and include bootstrap-grid.
    Signed-off-by: Andrey <NashyGrupp@gmail.com>

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle('Главная');
 ?>
-NashyGrupp
+ NashyGrupp
 <?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
